4493fcb8c06ff29dfe8e7102e313efdd1f8eb617
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2011-04-05 21:48:32 +0200

    Empty Titanium Module

---
+++ Classes/ComMathieuravauxTi_ios_couchbaseModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComMathieuravauxTi_ios_couchbaseModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComMathieuravauxTi_ios_couchbaseModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
